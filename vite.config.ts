fc0b18d95156a11cf69855657fd0bc712a47ff25
Md. Rakib Trofder <mdrakibtrofder@gmail.com>
2026-04-16 16:47:31 +0600

    feature: configuration for deploying at github

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+    base: "/lingua-trust/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
